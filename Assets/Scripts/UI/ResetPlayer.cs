97322ba1b3ea6df57f9320b49337feee20b2414b
Noam Sapir <noam1sapir@gmail.com>
2019-06-10 21:39:24 +0300

    Revert "Revert "GameJolt ranking working.""
    This reverts commit 452cf77cf7de38fa60c126a24bca4b39c96f5984.

--- Assets/Scripts/UI/ResetPlayer.cs
+++ Assets/Scripts/UI/ResetPlayer.cs
@@ -12,14 +12,23 @@ public class ResetPlayer : MonoBehaviour
     {
         //GameJolt.API.DataStore.Delete("TotalKills", true); //Values to reset player progress (Story when will be added..)
         //GameJolt.API.DataStore.Delete("HighScore", true);
-        GameJolt.API.DataStore.Delete("Rank", false);
-        GameJolt.API.DataStore.Delete("XP", false);
-        GameJolt.API.DataStore.Delete("XPtonextRank", false);
+        GameJolt.API.DataStore.Delete("Rank", false, (bool success) => { Debug.Log("Reseted"); });
+        GameJolt.API.DataStore.Delete("XP", false, (bool success) => { Debug.Log("Reseted"); });
+        GameJolt.API.DataStore.Delete("XPtonextRank", false, (bool success) => { Debug.Log("Reseted"); });
+        GameJolt.API.DataStore.Set("XP", 0.ToString(), false, (bool success) => { });
+        GameJolt.API.DataStore.Set("XPtonextRank", 100.ToString(), false, (bool success) => { });
+        GameJolt.API.DataStore.Set("Rank", 1.ToString(), false, (bool success) => { InvokeRepeating("Quit", 2.0f, 2.0f); });
+    }
+    private void Quit()
+    {
+        GameJolt.API.DataStore.Set("XP", 0.ToString(), false, (bool success) => { });
+        GameJolt.API.DataStore.Set("XPtonextRank", 100.ToString(), false, (bool success) => { });
+        GameJolt.API.DataStore.Set("Rank", 1.ToString(), false, (bool success) => { });
         GameJoltAPI.Instance.CurrentUser.SignOut();
-
         PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
         Debug.Log("All reseted.");
         Application.Quit();
     }
-  
+
 }
